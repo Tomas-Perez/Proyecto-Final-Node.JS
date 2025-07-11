9b21dff891886edc0fad795ac33371e39cc8a26c
Tomas-Perez <toperez@frba.utn.edu.ar>
2025-07-11 02:03:24 -0300

    Correcciones credenciales

--- src/data/data.js
+++ src/data/data.js
@@ -6,9 +6,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOM,
-  projectId: "api-rest-node-js-data-4ef84",
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "222871401216",
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID
 };
 
